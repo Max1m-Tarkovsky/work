0de632eb1042a26cf2a1cf2c0a0b3368524ba45c
Max1m-Tarkovsky <tarkovsky061295@gmail.com>
2020-03-10 21:34:37 +0200

    HW7

---
+++ src/com/ua/hillel/homeWork7/Main.java
@@ -0,0 +1,23 @@
+package com.ua.hillel.homeWork7;
+
+
+public class Main {
+    public static void main(String[] args) {
+        MyList myList = new MyList();
+        MyList myList1 = new MyList();
+        myList.add("Class");
+        myList.add("Home");
+        myList.add("floor");
+        System.out.println(myList.indexOf("Home"));
+        System.out.println(myList.delete("Home"));
+        System.out.println(myList.contains("Class"));
+        myList.add("Home");
+        System.out.println(myList.equals(myList1));
+        System.out.println(myList.get(1));
+        System.out.println(myList.getSize());
+        myList.clear();
+        System.out.println(myList);
+    }
+
+
+}
---
+++ src/com/ua/hillel/homeWork7/MyList.java
@@ -0,0 +1,104 @@
+package com.ua.hillel.homeWork7;
+
+import java.util.Arrays;
+
+
+public class MyList {
+
+    String[] myList = new String[0];
+
+
+    public boolean add(String value) {
+        if (value == null) {
+            return false;
+        }
+
+        myList = Arrays.copyOf(myList, myList.length + 1);
+        myList[myList.length - 1] = value;
+        return true;
+    }
+
+    public boolean delete(String value) {
+        if (value == null) {
+            return false;
+        }
+
+        for (int i = 0; i < myList.length; i++) {
+            if (value.equals(myList[i])) {
+                for (int j = i; j < myList.length - 1; j++) {
+                    myList[j] = myList[j + 1];
+                    myList = Arrays.copyOf(myList, myList.length - 1);
+                }
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+    public boolean contains(String value) {
+        if (value == null) {
+            return false;
+        }
+        for (int i = 0; i < myList.length; i++) {
+            if (value.equals(myList[i])) {
+                return true;
+            }
+        }
+
+
+        return false;
+    }
+
+    public boolean equals(String[] value) {
+        if (value == null) {
+            return false;
+        }
+        if (!(myList.length == (value.length))) {
+            return false;
+
+        }
+        for (int i = 0; i < myList.length; i++) {
+            if (!myList[i].equals(value[i])) ;
+            return false;
+        }
+        return true;
+    }
+
+
+    public void clear() {
+        myList = new String[0];
+
+    }
+
+
+    public int indexOf(String value) {
+        if (value == null) {
+            return -1;
+        }
+        for (int i = 0; i < myList.length; i++) {
+            if (value.equals(myList[i])) {
+                return i;
+            }
+
+        }
+        return -1;
+    }
+
+    public int getSize() {
+
+        return myList.length;
+    }
+
+    public String get(int value) {
+        return myList[value];
+
+
+    }
+
+
+}
+
+
+
