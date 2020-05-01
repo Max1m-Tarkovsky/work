6717f9733fc90120d98a9922c046ea6419b0e78c
Max1m-Tarkovsky <tarkovsky061295@gmail.com>
2020-05-01 14:00:02 +0300

    HW10

--- src/com/ua/hillel/homeWork10/QuadraticEquation.java
+++ src/com/ua/hillel/homeWork10/QuadraticEquation.java
@@ -2,9 +2,57 @@ package com.ua.hillel.homeWork10;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class QuadraticEquation {
-    public static double equation(double x, int n, int k) {
+    public static void main(String[] args) {
+
+
+        double a, b, c;
+        double D;
+
+        System.out.println("The program solves a quadratic equation of the form :");
+        System.out.println("ax^2 + bx + c = 0");
+        System.out.println("Pleas Enter  a, b и c:");
+        Scanner in = new Scanner(System.in);
+
+        a = in.nextDouble();
+        b = in.nextDouble();
+        c = in.nextDouble();
+
+
+        D = b * b - 4 * a * c;
+        if (D > 0) {
+            double x1, x2;
+            x1 = (-b - Math.sqrt(D)) / (2 * a);
+            x2 = (-b + Math.sqrt(D)) / (2 * a);
+            System.out.println("Root a quadratic : First  = " + x1 + ", Second  = " + x2);
+        }
+        else if (D == 0) {
+            double x;
+            x = -b / (2 * a);
+            System.out.println("The equation has a single root : x = " + x);
+        }
+        else {
+            System.out.println("The equation don't has root! ");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   /* public static double equation(double x, int n, int k) {
 
         return Math.sin(Math.pow(x, n)) + Math.pow(x, k) * x - 13;
 
@@ -27,11 +75,13 @@ public class QuadraticEquation {
     }
 
     public static void main(String[] args) {
-        System.out.println(decision(5, 8));
+
+        System.out.println(decision(2, 4));
+    }
+*/
+
+
     }
 
 
-
-
 }
-
